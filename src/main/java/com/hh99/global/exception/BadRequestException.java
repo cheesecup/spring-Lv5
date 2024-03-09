e074ae4d0763083c04466fbda8129d813deadc1a
cheesecup <kdy8252@naver.com>
2024-03-09 12:10:30 +0900

    exception 기능 추가

---
+++ src/main/java/com/hh99/global/exception/BadRequestException.java
@@ -0,0 +1,13 @@
+package com.hh99.global.exception;
+
+public class BadRequestException extends GlobalException{
+
+    public BadRequestException(ErrorMsg errorMsg) {
+        super(errorMsg.getMessage());
+    }
+
+    @Override
+    public int statusCode() {
+        return 400;
+    }
+}
